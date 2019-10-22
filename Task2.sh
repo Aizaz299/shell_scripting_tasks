43c2cdbf97e2b0813f501c7955ffdd1e0fbed060
Aizaz Ur Rehman <aziaz299@gmail.com>
2019-10-22 17:08:42 +0500

    Update Task2.sh

--- Task2.sh
+++ Task2.sh
@@ -1,3 +1,18 @@
+echo "Enter Two numbers : "
+read a 
+read b 
+  
+# Input type of operation 
+echo "Enter Choice :"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read ch 
+  
+# Switch Case to perform 
+# calulator operations 
+
 case $ch in
   1)res=`echo $a + $b | bc` 
   ;; 
