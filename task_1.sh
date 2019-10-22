fb0a7aa3dca2cbe2ef2b56dec090469dcfcc927b
Aizaz Ur Rehman <aziaz299@gmail.com>
2019-10-22 17:09:06 +0500

    Create task_1.sh

---
+++ task_1.sh
@@ -0,0 +1,14 @@
+#Write your code here
+echo "Enter three Integers:"
+read a b c
+
+if [ $a -gt $b -a $a -gt $c ]
+then
+	echo "$a is Greatest"
+elif [ $b -gt $c -a $b -gt $a ]
+then
+	echo "$b is Greatest"
+else
+	echo "$c is Greatest!"
+fi
+Output:
